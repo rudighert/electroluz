1b84f67658b08cad551c81ab1bc21c44deea1b64
Rudy iMac <rudighert@gmail.com>
2013-06-24 21:09:58 -0400

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Electroluz::Application.config.secret_token = 'e7b94d0dd3626311abb54e8103b5216f50e73a477ad4e046856d8036f6250a5838190c2bacdd383536e277d6bc40d6561f9c71f72a84144ca2706c2649e8295a'
